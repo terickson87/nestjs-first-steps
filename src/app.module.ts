65b2da9e450afa7082c83132d45f110bfe4aa14d
Todd Ericskon <todd.erickson87@gmail.com>
2024-04-30 10:32:42 -0700

    middleware completed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,21 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DogsModule } from './dogs/dogs.module';
 import { CatsModule } from './cats/cats.module';
+import { logger } from './common/middleware/logger.middleware';
+import helmet from 'helmet';
 
 @Module({
   imports: [DogsModule, CatsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+    .apply(helmet(),logger)
+    .forRoutes({ path: 'cats', method: RequestMethod.GET});
+  }
+  
+}
