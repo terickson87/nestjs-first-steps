cc0f7d625c6b473b8ffc00abf7d8826e4b55cbf7
Todd Ericskon <todd.erickson87@gmail.com>
2024-04-29 17:01:17 -0700

    completed modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CatsController } from './cats/cats.controller';
 import { DogsModule } from './dogs/dogs.module';
-import { CatsService } from './cats/cats.service';
+import { CatsModule } from './cats/cats.module';
 
 @Module({
-  imports: [DogsModule],
-  controllers: [AppController, CatsController],
-  providers: [AppService, CatsService],
+  imports: [DogsModule, CatsModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
